9078880e1732796a905812b83c249d242004535f
Karthik Thejendra <119528503+KarthikT23@users.noreply.github.com>
2024-08-23 22:27:04 +0530

    Add files via upload

---
+++ Tkinter Dashboard/data.py
@@ -0,0 +1,59 @@
+# Dictionary data for sales_data
+sales_data = {
+    "A": 15000,
+    "B": 12000,
+    "C": 18000,
+    "D": 20000,
+    "E": 17000,
+    "F": 16000
+}
+
+# Dictionary data for inventory_data
+inventory_data = {
+    "Product_A": 500,
+    "Product_B": 350,
+    "Product_C": 420,
+    "Product_D": 280,
+    "Product_E": 600,
+    "Product_F": 320
+}
+
+# Dictionary data for product_data
+product_data = {
+    "A": 15.5,
+    "B": 20.0,
+    "C": 25.0,
+    "D": 10.5,
+    "E": 18.0,
+    "F": 11.0
+}
+
+
+# Dictionary data for sales_year_data
+sales_year_data = {
+    2015: 150000,
+    2016: 155000,
+    2017: 148000,
+    2018: 160000,
+    2019: 158000,
+    2020: 165000,
+    2021: 162000,
+    2022: 170000,
+    2023: 168000,
+    2024: 175000
+}
+# Dictionary data for inventory_month_data
+inventory_month_data = {
+    "Jan": 500,
+    "Feb": 350,
+    "Mar": 420,
+    "Apr": 280,
+    "May": 600,
+    "Jun": 320,
+    "Jul": 450,
+    "Aug": 400,
+    "Sep": 390,
+    "Oct": 470,
+    "Nov": 500,
+    "Dec": 480
+}
